5cd4eb53e1dd1ab18eb88e807526508937646c67
Leniggo <n.eberl79@gmail.com>
2024-01-16 02:05:33 +0100

    fix dto

--- src/modules/group/dto/create-group.dto.ts
+++ src/modules/group/dto/create-group.dto.ts
@@ -1,7 +1,4 @@
 import { PickType } from '@nestjs/swagger';
 import GroupDTO from './group.dto';
 
-export default class CreateGroupDTO extends PickType(GroupDTO, [
-  'name',
-  'userSaldo',
-]) {}
+export default class CreateGroupDTO extends PickType(GroupDTO, ['name']) {}
